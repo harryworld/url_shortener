0817f3e3b3d2048a45936f848b91ca4f1788db2f
Harry Ng <harryworld@gmail.com>
2014-06-29 21:55:20 +0800

    Use html to show site index

--- spec/features/visit_site_spec.rb
+++ spec/features/visit_site_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 feature 'visit the site' do
-  scenario 'see the Hello World' do
+  scenario 'see the Hi, there!' do
     visit '/'
-    expect(page).to have_content 'Hello World!'
+    expect(page).to have_content 'Hi, there!'
+  end
+
+  scenario 'show HTML page' do
+    visit '/'
+    expect(page).to have_selector('html')
   end
 end
