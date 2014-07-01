358ffaf3284e46cc0a3ab9784800f00aaeb3b46b
Harry Ng <harryworld@gmail.com>
2014-07-02 01:58:11 +0800

    Add RSpec test case

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Url, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a url" do
+    url = Url.new
+    url.original = "http://www.google.com/"
+    url.save
+    expect(url).to be_valid
+  end
 end
