27b714de7ec0eac3e749f85fede8398614857250
Harry Ng <harryworld@gmail.com>
2014-07-08 14:34:27 +0800

    Add url_rspec

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,22 @@
 class Url < ActiveRecord::Base
+
+  before_save :generate_url
+  validates :original, :presence => true
+
+  def generate_url
+    self.converted = "http://localhost:3000/#{random_string}"
+  end
+
+  def check_converted
+    if self.converted.nil?
+      self.errors.add :base, "the converted URL should exists"
+    end
+  end
+
+  private
+
+    def random_string
+      (0...6).map { (65 + rand(26)).chr }.join
+    end
+
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe Url, :type => :model do
     url.save
     expect(url).to be_valid
   end
+
+  it "is invalid without original" do
+    url = Url.new
+    url.save
+    expect(url).to be_invalid
+  end
+
+  it "is having 6 characters in random string" do
+    url = Url.new
+    str = url.instance_eval{ random_string }
+    expect(str.length).to eq 6
+  end
+
+  it "is should have converted url" do
+    url = Url.new
+    url.original = "http://www.google.com/"
+    url.save
+    expect(url.converted).not_to be_nil
+  end
 end
